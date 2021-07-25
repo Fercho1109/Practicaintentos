3a7fac51d151741771907c6295ed968282ead898
Fercho1109 <87920201+Fercho1109@users.noreply.github.com>
2021-07-24 21:40:10 -0500

    Add files via upload
    v

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:registro_agua/paginas/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
---
+++ registro_agua/android/app/src/main/kotlin/com/example/registro_agua/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.registro_agua
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
